22d68161cfd80ebbbd56fdbfbeac9836873092c5
hash <91471322+harshkhavale@users.noreply.github.com>
2022-11-05 18:08:32 +0530

    Delete dbo.SubCatTbl.data.sql

--- dbo.SubCatTbl.data.sql
+++
@@ -1,5 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[SubCatTbl] ON
-INSERT INTO [dbo].[SubCatTbl] ([SubCatId], [CatId], [SubCategory]) VALUES (1, 2, N'C-Type')
-INSERT INTO [dbo].[SubCatTbl] ([SubCatId], [CatId], [SubCategory]) VALUES (2, 3, N'Wireless')
-SET IDENTITY_INSERT [dbo].[SubCatTbl] OFF
-Truncate table SubCatTbl;

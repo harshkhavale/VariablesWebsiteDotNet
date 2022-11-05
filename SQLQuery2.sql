b2c7b1b49f8e9bc6ff741c507465f9f89f70b839
hash <91471322+harshkhavale@users.noreply.github.com>
2022-11-05 18:09:53 +0530

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE BindAllProducts
-AS
-Select A.*,B.*,A.ProductPrice-A.ProductSellingPrice as DiscAmount,B.Name as ImageName,C.BrandName as BrandName from ProductDataTbl A
-INNER JOIN BrandNameTbl C ON C.Id = A.ProductBrandId
-
-CROSS APPLY(SELECT TOP 1 * FROM ProductImgTbl B WHERE B.PId = A.ProductId ORDER BY B.PId DESC)B
-ORDER BY A.ProductId DESC 
-Return 0;
